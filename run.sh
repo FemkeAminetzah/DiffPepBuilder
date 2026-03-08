d06e93c2b4613f77a9a4185886e8e1ba26a757cb
Femke Aminetzah <femkeaminetzah2003@gmail.com>
2026-03-08 13:54:33 +0100

    1SHY Diffpepbuilder run

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 uv run python run_diffpepbuilder.py \
-  --pdb examples/receptor_data/5HO6_cMET_only.pdb \
-  --hotspots "A1106,A1159,A1180,A1184" \
+  --pdb examples/receptor_data/1SHY_cMET.pdb \
+  --hotspots "B124,B159,B190" \
   --min_length 12 \
-  --max_length 16 \
-  --samples_per_length 4 \
-  --build_ss_bond
+  --max_length 30 \
+  --samples_per_length 10 \
+  --build_ss_bond \
+  --amber_relax \
+  --rosetta_relax
